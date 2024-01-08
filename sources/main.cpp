990bfb16a826461b0f7928e6331250968f07ac37
ppoliakov <pasha.u.polyak@gmail.com>
2024-01-08 12:34:11 +0300

    add sfml basics

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,5 +1,21 @@
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+    window.setFramerateLimit(60);
+
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.display();
+    }
+
     return 0;
 }
--- sources/pch.h
+++ sources/pch.h
@@ -1,3 +1,5 @@
 #pragma once
 
 #include <iostream>
+
+#include <SFML/Window.hpp>
