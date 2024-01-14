68565fac78282a069a605ccbece7702869bb0540
ppoliakov <pasha.u.polyak@gmail.com>
2024-01-14 12:47:13 +0300

    add VM to simplify usage of Processor

--- sources/processor/vm/virtual_machine.cpp
+++ sources/processor/vm/virtual_machine.cpp
@@ -1,1 +1,41 @@
 #include "virtual_machine.h"
+#include "processor/processor.h"
+#include "processor/processor_control_block.h"
+
+#include <windows.h>
+#include <debugapi.h>
+
+static void DebugInvalidCommandTrap()
+{
+    DebugBreak();
+}
+
+bool VirtualMachine::RegisterProcedure(std::unique_ptr<ProcedureBase> procedure, uint8_t inputArgs, uint8_t outputArgs)
+{
+    ProcedureInfo info = ProcedureInfo { inputArgs, outputArgs, procedure.get() };
+    const ProcedureId id = _procedureTable.RegisterProcedure(info);
+    if (id == ProcedureId::Invalid) {
+        return false;
+    }
+    _procedureOwner.emplace_back(std::move(procedure));
+    return true;
+}
+
+std::optional<ProcessorContext> VirtualMachine::MakeProcessorContext(Memory memory)
+{
+    if (!memory.HasBytes<ProcessorControlBlock>()) {
+        return {};
+    }
+
+    auto& controlBlock = memory.Get<ProcessorControlBlock>();
+
+    ProcessorContext context {
+        _procedureTable,
+        controlBlock,
+        memory
+    };
+
+    context.SetInvalidCommandTrap(&DebugInvalidCommandTrap);
+
+    return context;
+}
--- sources/processor/vm/virtual_machine.h
+++ sources/processor/vm/virtual_machine.h
@@ -3,10 +3,11 @@
 #include "procedures/procedure.h"
 #include "procedures/procedure_table.h"
 
-class VirtualMachine
-{
+class VirtualMachine {
 public:
-    bool RegisterProcedure(std::unique_ptr<ProcedureBase> procedure);
+    bool RegisterProcedure(std::unique_ptr<ProcedureBase> procedure, uint8_t inputArgs, uint8_t outputArgs);
+
+    std::optional<ProcessorContext> MakeProcessorContext(Memory memory);
 
 private:
     std::vector<std::unique_ptr<ProcedureBase>> _procedureOwner;
