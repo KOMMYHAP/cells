bd962f1deedfe9b02e995554228aa0e188efb339
ppoliakov <pasha.u.polyak@gmail.com>
2024-01-27 19:51:17 +0300

    replace deprecated gl_FragColor with layout(location=0) out variable

--- sources/shaders/fragment_shader.glsl
+++ sources/shaders/fragment_shader.glsl
@@ -1,6 +1,8 @@
 uniform sampler2D texture;
 
+layout(location = 0) out vec4 outColor;
+
 void main()
 {
-    gl_FragColor = texture2D(texture, gl_TexCoord[0].xy);
+    outColor = texture2D(texture, gl_TexCoord[0].xy);
 }
