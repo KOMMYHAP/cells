d103ba52af690e735841abc7b201de94a93829bd
ppoliakov <pasha.u.polyak@gmail.com>
2024-02-05 22:39:36 +0300

    add mutation

--- sources/simulation/systems/selection_system.cpp
+++ sources/simulation/systems/selection_system.cpp
@@ -1,4 +1,5 @@
 #include "selection_system.h"
+#include "processor/processor_control_block.h"
 
 #include "components/cell_id.h"
 #include "genetic/crossover_algorithm.h"
@@ -30,6 +31,7 @@ SelectionSystem::Result SelectionSystem::TickGeneration()
     result.cellFactory = &_selectionPresetFactory;
     result.parentBrains = &selection.brains;
     result.shouldRespawn = true;
+    return result;
 }
 
 void SelectionSystem::MakeNextGeneration()
@@ -67,6 +69,9 @@ void SelectionSystem::Restart()
 
 std::vector<CellBrain> SelectionSystem::CollectBestBrains() const
 {
+    std::vector<CellBrain> parentBrains;
+    parentBrains.reserve(_bestCellsLimit);
+
     std::vector<CellId> aliveCellIds;
     aliveCellIds.reserve(_idSystem.GetCellsCount());
     _idSystem.Iterate([&](const CellId id) {
@@ -76,11 +81,35 @@ std::vector<CellBrain> SelectionSystem::CollectBestBrains() const
     std::shuffle(aliveCellIds.begin(), aliveCellIds.end(), common::GetRandomEngine());
     const uint16_t bestCellsCount = std::min<uint16_t>(_bestCellsLimit, aliveCellIds.size());
 
-    std::vector<CellBrain> parentBrains;
-    parentBrains.reserve(bestCellsCount);
     std::ranges::transform(std::span(aliveCellIds).first(bestCellsCount), std::back_inserter(parentBrains), [&](const CellId id) {
         return _brainSystem.Get(id);
     });
+
+    if (bestCellsCount < _bestCellsLimit) {
+        std::vector<CellBrain> copies = parentBrains;
+        const uint32_t fullCopies = (_bestCellsLimit - bestCellsCount) / copies.size();
+        for (uint32_t copyIndex = 0; copyIndex < fullCopies; ++copyIndex) {
+            parentBrains.insert(parentBrains.end(), copies.begin(), copies.end());
+        }
+        const uint32_t restCopies = (_bestCellsLimit - bestCellsCount) - fullCopies * bestCellsCount;
+        assert(restCopies < copies.size());
+        for (uint32_t i = 0; i < restCopies; ++i) {
+            parentBrains.emplace_back(copies[i]);
+        }
+    }
+
+    std::uniform_int_distribution<size_t> mutation { 0, parentBrains.size() - 1 };
+    const size_t mutationIndex = mutation(common::GetRandomEngine());
+    CellBrain& brain = parentBrains[mutationIndex];
+    ProcessorMemory memory { brain.data };
+
+    std::uniform_int_distribution<uint16_t> brainMutation { sizeof(ProcessorControlBlock), static_cast<uint16_t>(memory.Size() - 1) };
+    const uint8_t brainMutationIndex = static_cast<uint8_t>(brainMutation(common::GetRandomEngine()));
+    memory.Move(brainMutationIndex + sizeof(ProcessorControlBlock));
+
+    std::uniform_int_distribution<uint16_t> dataMutation { 0, 255 };
+    const uint8_t data = static_cast<uint8_t>(dataMutation(common::GetRandomEngine()));
+    memory.Write(data);
 
     return parentBrains;
 }
