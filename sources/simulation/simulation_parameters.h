9e6eef98b9a84a189fcccbeeedd8099d677ba8c7
ppoliakov <pasha.u.polyak@gmail.com>
2024-05-09 17:23:37 +0300

    wip

--- sources/simulation/simulation_parameters.h
+++ sources/simulation/simulation_parameters.h
@@ -3,8 +3,11 @@
 #include "processor/processor_state.h"
 
 struct SimulationParameters {
+    common::Storage systems;
     sf::Time targetSimulationTime;
     uint8_t cellsCountPercentOfLimit { 0 };
+
+    // virtual machine
     uint8_t systemInstructionPerStep { 0 };
     ProcessorStateWatcher processorStateWatcher;
 };
--- sources/simulation/world_parameters.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "storage.h"
-
-struct WorldParameters {
-    common::Storage systems;
-};
