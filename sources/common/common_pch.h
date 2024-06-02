5d95ba3fde278259bf03c69115fe7f2b574acec4
ppoliakov <pasha.u.polyak@gmail.com>
2024-06-02 13:07:14 +0300

    add gsl

--- sources/common/common_pch.h
+++ sources/common/common_pch.h
@@ -1,3 +1,7 @@
 #pragma once
 
 #include "basic_defines.h"
+
+#include <gsl/assert>      // Ensures/Expects
+#include <gsl/pointers>    // owner, not_null
+#include <gsl/util>        // finally()/narrow_cast()...
