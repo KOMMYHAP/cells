9d26ce5444f0198c3677db34fb17854379aab4e4
ppoliakov <pasha.u.polyak@gmail.com>
2024-06-29 16:13:34 +0300

    add RandomAccessor utility to hide mutex as details, add Shuffle to interface

--- sources/common/random.cpp
+++ sources/common/random.cpp
@@ -2,29 +2,41 @@
 
 #include "mt/spin_lock.h"
 
-namespace {
-
-struct RandomData {
-    common::RandomEngine engine;
-    SpinLock spinlock;
-};
-RandomData* g_randomData { nullptr };
-
-}
+static common::RandomData* g_randomData { nullptr };
 
 namespace common {
 
+struct RandomData {
+    common::RandomEngine engine;
+    std::mutex lock;
+};
+
 float GetNextValue()
 {
-    SpinLocker _ { g_randomData->spinlock };
+    std::scoped_lock _ { g_randomData->lock };
     std::uniform_real_distribution dist { 0.0f, 1.0f };
     return dist(g_randomData->engine);
+}
+
+RandomAccessor::RandomAccessor()
+{
+    g_randomData->lock.lock();
+}
+
+RandomAccessor::~RandomAccessor()
+{
+    g_randomData->lock.unlock();
+}
+
+RandomEngine& RandomAccessor::AccessEngine()
+{
+    return g_randomData->engine;
 }
 
 void InitRandom(ApplicationStorage& storage, std::string_view seed)
 {
     auto seedSequence = std::seed_seq { seed.begin(), seed.end() };
-    g_randomData = &storage.Store<RandomData>(RandomEngine(seedSequence), SpinLock {});
+    g_randomData = &storage.Store<RandomData>(RandomEngine(seedSequence), std::mutex {});
 }
 
 void TermRandom()
--- sources/common/random.h
+++ sources/common/random.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "application_storage.h"
+#include <xutility>
 
 namespace common {
+
+struct RandomData;
 
 using RandomEngine = std::default_random_engine;
 
@@ -9,5 +12,24 @@ void InitRandom(ApplicationStorage& storage, std::string_view seed);
 void TermRandom();
 
 float GetNextValue();
+
+class RandomAccessor {
+public:
+    RandomAccessor();
+    RandomAccessor(const RandomAccessor&) = delete;
+    RandomAccessor(RandomAccessor&&) = delete;
+    RandomAccessor& operator=(const RandomAccessor&) = delete;
+    RandomAccessor& operator=(RandomAccessor&&) = delete;
+    ~RandomAccessor();
+
+    RandomEngine& AccessEngine();
+};
+
+template <std::ranges::random_access_range Ranges>
+    requires std::permutable<std::ranges::iterator_t<Ranges>>
+void Shuffle(Ranges&& ranges)
+{
+    std::ranges::shuffle(std::forward<Ranges>(ranges), RandomAccessor().AccessEngine());
+}
 
 }
