a61963a824bf85607a045a34fd21771d431a15fc
ppoliakov <pasha.u.polyak@gmail.com>
2024-06-29 17:06:29 +0300

    - extract random to folder - move interface to Accessor - remove global objects

---
+++ sources/common/random/random.h
@@ -0,0 +1,37 @@
+#pragma once
+
+namespace random {
+
+using Engine = std::default_random_engine;
+
+Engine MakeEngine(std::string_view seed);
+
+class Accessor {
+public:
+    Accessor(Engine& engine);
+    Accessor(const Accessor&) = default;
+    Accessor(Accessor&&) = default;
+    Accessor& operator=(const Accessor&) = default;
+    Accessor& operator=(Accessor&&) = default;
+
+    // Returns value from range [0.0f; 1.0f]
+    float GetNextValue();
+
+    // Returns value from range [start; end]
+    uint32_t GetValue(uint32_t start, uint32_t end);
+
+    template <std::ranges::random_access_range Ranges>
+        requires std::permutable<std::ranges::iterator_t<Ranges>>
+    void Shuffle(Ranges&& ranges);
+
+    template <std::ranges::constant_range Range>
+        requires std::ranges::sized_range<Range>
+    auto Select(Range range);
+
+private:
+    gsl::not_null<Engine*> _engine;
+};
+
+}
+
+#include "random.hpp"
