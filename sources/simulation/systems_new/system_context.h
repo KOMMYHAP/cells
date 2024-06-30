f5e8bebacfb4cddeb1375ddcf707e4d979f8a5a9
ppoliakov <pasha.u.polyak@gmail.com>
2024-06-30 23:20:43 +0300

    wip

--- sources/simulation/systems_new/system_context.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "components/cell_id.h"
-
-struct SystemContext {
-    CellId id;
-    std::span<std::byte*> components;
-};
--- sources/simulation/systems_new/system_handle.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-enum class SystemHandle : uint16_t {
-    Invalid = std::numeric_limits<uint16_t>::max()
-};
