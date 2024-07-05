c1cc98e8e98a691b1bb7e37a1ac5557d0042a4c4
ppoliakov <pasha.u.polyak@gmail.com>
2024-07-06 02:45:37 +0300

    wip

--- sources/cmake/target_copy_required_dll.cmake
+++ sources/cmake/target_copy_required_dll.cmake
@@ -2,14 +2,19 @@
 function(target_copy_required_dll)
     if (NOT WIN32)
     endif()
-
-    #set(asan-target-hack $ENV{VCToolsInstallDir}/bin/Hostx64/x64/clang_rt.asan_dynamic-x86_64.dll)
+    
+    if (NOT DEFINED ENV{VS_ASAN_PATH})
+        # E.g. C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.40.33807\bin\Hostx64\x64
+        message(FATAL_ERROR "You should specifiy environment variable VS_ASAN_PATH")
+    endif()
+    
+    set(asan-target $ENV{VS_ASAN_PATH}/clang_rt.asan_dynamic-x86_64.dll)
     add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
             COMMAND ${CMAKE_COMMAND}
             -E copy
             -t $<TARGET_FILE_DIR:${PROJECT_NAME}>
             $<TARGET_RUNTIME_DLLS:${PROJECT_NAME}>
-            #$<$<BOOL:${ENABLE_ASAN}>:${asan-target-hack}>
+            $<$<BOOL:${ENABLE_ASAN}>:${asan-target}>
             COMMAND_EXPAND_LISTS
     )
 endfunction()
