81ce8ec90b6f0246fae177efb7e4613969cd0aa0
ppoliakov <pasha.u.polyak@gmail.com>
2024-08-25 00:09:40 +0300

    remove dead code

--- sources/simulation/components/component.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-struct Component {
-    std::string_view name;
-    uint8_t numbersCount { 0 };
-    uint16_t sizeInBytes { 0 };
-};
--- sources/simulation/components/component_handle.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-enum class ComponentHandle : uint16_t {
-    Invalid = std::numeric_limits<uint16_t>::max()
-};
