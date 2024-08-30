63a43145641d522503c346db2078f80d350d9305
ppoliakov <pasha.u.polyak@gmail.com>
2024-08-31 00:59:51 +0300

    [REFACTOR] remove old code

--- sources/simulation/components/cell_id.h
+++ sources/simulation/components/cell_id.h
@@ -3,5 +3,3 @@
 enum class CellId : uint32_t {
     Invalid = std::numeric_limits<uint32_t>::max()
 };
-
-constexpr auto CellIdToInt(CellId id) { return static_cast<std::underlying_type_t<CellId>>(id); }
