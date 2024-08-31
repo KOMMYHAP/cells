1dbb3508a6e706f7bcb78b3894f34cb3a88cf4ab
ppoliakov <pasha.u.polyak@gmail.com>
2024-08-31 20:52:12 +0300

    [FIX] fps calculation (use milliseconds instead of seconds)

--- sources/ui/widgets/fps_widget.cpp
+++ sources/ui/widgets/fps_widget.cpp
@@ -4,7 +4,7 @@ void FpsWidget::UpdateWidget(sf::Time elapsedTime)
 {
     _frameTimeCounter.AddSample(elapsedTime.asMilliseconds());
     const int32_t frameDuration = _frameTimeCounter.CalcMedian();
-    const int32_t fps = static_cast<int32_t>(std::round(1.0f / static_cast<float>(frameDuration)));
+    const int32_t fps = static_cast<int32_t>(std::round(1000.0f / static_cast<float>(frameDuration)));
     ImGui::Text("FPS %3d", fps);
     ImGui::Text("Time %5d [ms]", frameDuration);
 }
