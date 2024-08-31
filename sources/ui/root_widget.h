a174b9ce6fb978ebbcf28df538bff2a7ad3dd119
ppoliakov <pasha.u.polyak@gmail.com>
2024-08-31 21:40:13 +0300

    [REFACTOR] extract template method impl to .hpp

--- sources/ui/root_widget.h
+++ sources/ui/root_widget.h
@@ -20,15 +20,4 @@ private:
     std::vector<CustomRenderWidget*> _customRenderWidgets;
 };
 
-template <class WidgetType, class... Args>
-    requires std::is_constructible_v<WidgetType, Args...>
-WidgetType& RootWidget::AddWidget(Args&&... args)
-{
-    auto widget = std::make_unique<WidgetType>(std::forward<Args>(args)...);
-    WidgetType& widgetRef = *widget;
-    _widgets.emplace_back(std::move(widget));
-    if constexpr (std::is_base_of_v<CustomRenderWidget, WidgetType>) {
-        _customRenderWidgets.emplace_back(&widgetRef);
-    }
-    return widgetRef;
-}
+#include "root_widget.hpp"
---
+++ sources/ui/root_widget.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "root_widget.h"
+
+template <class WidgetType, class... Args>
+    requires std::is_constructible_v<WidgetType, Args...>
+WidgetType& RootWidget::AddWidget(Args&&... args)
+{
+    auto widget = std::make_unique<WidgetType>(std::forward<Args>(args)...);
+    WidgetType& widgetRef = *widget;
+    _widgets.emplace_back(std::move(widget));
+    if constexpr (std::is_base_of_v<CustomRenderWidget, WidgetType>) {
+        _customRenderWidgets.emplace_back(&widgetRef);
+    }
+    return widgetRef;
+}
