46131c930125a90cd2c03ef446ee611459270c37
ppoliakov <pasha.u.polyak@gmail.com>
2024-09-01 15:21:33 +0300

    [FEATURE] add debug test menu widget with states logging

---
+++ sources/ui/menu_widgets/test_menu_widget.cpp
@@ -0,0 +1,36 @@
+﻿#include "test_menu_widget.h"
+
+void TestMenuWidget::OnMenuItemOpenedFirstTime()
+{
+    std::cout << "OnMenuItemOpenedFirstTime\n";
+}
+
+void TestMenuWidget::OnMenuItemJustOpened()
+{
+    std::cout << "OnMenuItemJustOpened\n";
+}
+
+BaseMenuWidget::MenuWidgetAction TestMenuWidget::ProcessMenuItem(sf::Time elapsedTime)
+{
+    _elapsedTimeSinceFlush += elapsedTime;
+    static const sf::Time FlushPeriod { sf::seconds(1) };
+    if (_elapsedTimeSinceFlush > FlushPeriod) {
+        std::cout << "ProcessMenuItem\n";
+        Flush();
+    }
+    ImGui::Text("Elapsed time since last flush: %4d [ms]", _elapsedTimeSinceFlush.asMilliseconds());
+    return MenuWidgetAction::KeepOpen;
+}
+
+void TestMenuWidget::OnMenuItemJustClosed()
+{
+    std::cout << "OnMenuItemJustClosed\n";
+    // Force flush on close, because there will not be any processing for this widget.
+    Flush();
+}
+
+void TestMenuWidget::Flush()
+{
+    _elapsedTimeSinceFlush = {};
+    std::cout.flush();
+}
---
+++ sources/ui/menu_widgets/test_menu_widget.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "base_menu_widget.h"
+
+class TestMenuWidget final : public BaseMenuWidget {
+public:
+    void OnMenuItemOpenedFirstTime() override;
+    void OnMenuItemJustOpened() override;
+    MenuWidgetAction ProcessMenuItem(sf::Time elapsedTime) override;
+    void OnMenuItemJustClosed() override;
+
+private:
+    void Flush();
+    
+    sf::Time _elapsedTimeSinceFlush;
+};
