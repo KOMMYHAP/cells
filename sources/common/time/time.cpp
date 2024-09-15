f1cb330814d2198558572f0af21b806f7c5da7ae
ppoliakov <pasha.u.polyak@gmail.com>
2024-09-15 22:50:21 +0300

    [REFACTOR] hide sfml::system usage
    part 4: implement sf::Time

--- sources/common/time/time.cpp
+++ sources/common/time/time.cpp
@@ -1,1 +1,48 @@
 ﻿#include "time.h"
+
+#include "utils/narrow_cast.h"
+
+namespace Common {
+
+Time Time::FromSeconds(float seconds)
+{
+    ASSERT(seconds >= 0.0f, "negative time");
+    const float us = seconds * 1000.0f * 1000.0f;
+    const int classification = std::fpclassify(us);
+    ASSERT(classification == FP_ZERO || classification == FP_NORMAL, "overflow");
+    const int64_t roundedMicroseconds = std::llround(us);
+    return Time { NarrowCast<uint64_t>(roundedMicroseconds) };
+}
+
+Time::Time(uint64_t microseconds)
+    : _microseconds(microseconds)
+{
+}
+
+Time Time::FromMilliseconds(uint64_t ms)
+{
+    ASSERT(ms <= std::numeric_limits<uint64_t>::max() / 1000, "overflow");
+    return Time { ms * 1000ull };
+}
+
+Time Time::FromMicroseconds(uint64_t us)
+{
+    return Time { us };
+}
+
+float Time::AsSeconds() const
+{
+    return static_cast<float>(_microseconds) / 1000.0f / 1000.0f;
+}
+
+uint64_t Time::AsMilliseconds() const
+{
+    return NarrowCast<uint32_t>(_microseconds / 1000);
+}
+
+uint64_t Time::AsMicroseconds() const
+{
+    return _microseconds;
+}
+
+}
--- sources/common/time/time.h
+++ sources/common/time/time.h
@@ -1,6 +1,23 @@
 ﻿#pragma once
 
+namespace Common {
 
-namespace common {
+class Time {
+public:
+    static Time FromSeconds(float seconds);
+    static Time FromMilliseconds(uint64_t ms);
+    static Time FromMicroseconds(uint64_t us);
+
+    float AsSeconds() const;
+    uint64_t AsMilliseconds() const;
+    uint64_t AsMicroseconds() const;
+
+    static const inline Time Zero { 0 };
+
+private:
+    explicit Time(uint64_t microseconds);
+
+    uint64_t _microseconds { 0 };
+};
 
 }
