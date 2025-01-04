a6b54c78582f247b5d19974ca612af13e531092c
ppoliakov <pasha.u.polyak@gmail.com>
2025-01-05 02:03:19 +0300

    apply formatter for cmake files

--- sources/cmake/target_copy_required_dll.cmake
+++ sources/cmake/target_copy_required_dll.cmake
@@ -1,13 +1,13 @@
 # Copy DLLs needed for runtime on Windows
 function(target_copy_required_dll)
     if (NOT WIN32)
-    endif()
-    
+    endif ()
+
     if (NOT DEFINED ENV{VS_ASAN_PATH})
         # E.g. C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.40.33807\bin\Hostx64\x64
         message(FATAL_ERROR "You should specifiy environment variable VS_ASAN_PATH")
-    endif()
-    
+    endif ()
+
     set(asan-target $ENV{VS_ASAN_PATH}/clang_rt.asan_dynamic-x86_64.dll)
     add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
             COMMAND ${CMAKE_COMMAND}
