9e64979bc5bbe55001ce1a218d29c834983f0cc7
ppoliakov <pasha.u.polyak@gmail.com>
2025-01-05 23:07:15 +0300

    bind root_widget and invalid_widget

--- sources/scripts/bind/lua_menu.h
+++ sources/scripts/bind/lua_menu.h
@@ -11,6 +11,9 @@ public:
 
     std::pair<std::underlying_type_t<MenuWidgetId>, LuaMenuWidget*> Register(sol::stack_object luaParentWidget, std::string_view name);
 
+    uint32_t RootWidget {std::to_underlying(MenuWidgetId::Root)};
+    uint32_t InvalidWidget {std::to_underlying(MenuWidgetId::Invalid)};
+
 private:
     gsl::not_null<LuaLogger*> _logger;
     gsl::not_null<MenuRootWidget*> _menuRootWidget;
--- sources/scripts/system/lua_system.cpp
+++ sources/scripts/system/lua_system.cpp
@@ -10,14 +10,18 @@ LuaSystem::LuaSystem(MenuRootWidget& menuRootWidget, LuaLogger& logger)
 {
     _luaState.open_libraries(sol::lib::base);
 
-    _luaState.new_usertype<LuaMenuWidget>("menu_widget",
-        sol::meta_function::new_index,
-        &LuaMenuWidget::OverrideFunction);
+    {
+        _luaState.new_usertype<LuaMenuWidget>("menu_widget",
+            sol::meta_function::new_index,
+            &LuaMenuWidget::OverrideFunction);
+    }
+    {
+        sol::usertype<LuaMenu> luaMenuTable = _luaState.new_usertype<LuaMenu>("menu",
+            "register", &LuaMenu::Register,
+            "root_widget", sol::readonly(&LuaMenu::RootWidget),
+            "invalid_widget", sol::readonly(&LuaMenu::InvalidWidget));
+    }
 
-    _luaState.new_usertype<LuaMenu>("menu",
-        "register", &LuaMenu::Register,
-        "root_widget", std::to_underlying(MenuWidgetId::Root),
-        "invalid_widget", std::to_underlying(MenuWidgetId::Invalid));
     _luaState["ui_menu"] = LuaMenu(*_logger, *_menuRootWidget);
 
     sol_ImGui::Init(_luaState);
@@ -25,7 +29,7 @@ LuaSystem::LuaSystem(MenuRootWidget& menuRootWidget, LuaLogger& logger)
 
 sol::function_result LuaSystem::RunScript(std::string_view script)
 {
-    sol::function_result result = _luaState.script(script, sol::default_traceback_error_handler);
+    sol::function_result result = _luaState.script(script);
     if (!result.valid()) {
         const sol::error error = result;
         _logger->Error("Failed to execute script!\n{}", error.what());
