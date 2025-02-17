385a372ac90cc5101afc7cdffb4c1c06a498c892
ppoliakov <pasha.u.polyak@gmail.com>
2025-02-18 01:46:54 +0300

    add operator<=> for CellPosition

--- sources/simulation/cell_position_utils.h
+++ sources/simulation/cell_position_utils.h
@@ -1,17 +1,4 @@
 #pragma once
 #include "components/generated/auto_cell_position.h"
 
-inline bool operator==(const CellPosition& lhs, const CellPosition& rhs)
-{
-    return lhs.x == rhs.x && lhs.y == rhs.y;
-}
-
-inline bool operator<(const CellPosition& lhs, const CellPosition& rhs)
-{
-    if (lhs.x == rhs.x) {
-        return lhs.y < rhs.y;
-    }
-    return lhs.x < rhs.x;
-}
-
 inline constexpr CellPosition InvalidCellPosition { CellPosition::Invalid, CellPosition::Invalid };
