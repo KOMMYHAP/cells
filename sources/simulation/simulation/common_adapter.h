3e7ec8443496b5d539293f26f75b304e06481962
ppoliakov <pasha.u.polyak@gmail.com>
2025-06-29 22:05:56 +0300

    add RandomEngine alias for code generator

---
+++ sources/simulation/simulation/common_adapter.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "storage/stack_storage.h"
+#include "random/random.h"
+
+using SimulationStorage = common::StackStorage;
+using RandomEngine = Random::Engine;
--- sources/simulation/simulation/simulation_storage.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "storage/stack_storage.h"
-
-using SimulationStorage = common::StackStorage;
