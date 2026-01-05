8e43b0f4b6ad908c7205ad877684bd96da7445ae
ppoliakov <pasha.u.polyak@gmail.com>
2026-01-05 04:11:35 +0300

    fix rules of conway game

--- sources/conway_life/conway_game.cpp
+++ sources/conway_life/conway_game.cpp
@@ -17,19 +17,26 @@ ConwayGameSummary ConwayGame::DoNextGeneration()
 {
     ConwayGameSummary summary;
 
-    for (int32_t x = 0; x < _currentField.GetSizeX(); ++x) {
-        for (int32_t y = 0; y < _currentField.GetSizeY(); ++y) {
+    for (int32_t y = 0; y < _currentField.GetSizeY(); ++y) {
+        for (int32_t x = 0; x < _currentField.GetSizeX(); ++x) {
             const FieldPosition position { x, y };
+            const bool hasLife = _currentField.Has(position);
             const int32_t neighboursCount = _currentField.GetNeighboursCount(position);
-            const bool willAlive = neighboursCount == 2 || neighboursCount == 3;
 
-            summary.aliveCells += willAlive ? 1 : 0;
-            _nextField.Set(position, willAlive);
+            if (!hasLife && neighboursCount == 3) {
+                summary.aliveCells += 1;
+                _nextField.Set(position, true);
+            } else if (hasLife && (neighboursCount == 2 || neighboursCount == 3)) {
+                summary.aliveCells += 1;
+                _nextField.Set(position, true);
+            } else {
+                _nextField.Set(position, false);
+            }
         }
     }
     _currentField = _nextField;
 
-    auto [similarFieldIt, inserted] = _previousGenerations.emplace(_nextField, FieldDescription { _currentGenerationNumber });
+    auto [similarFieldIt, inserted] = _previousGenerations.emplace(_currentField, FieldDescription { _currentGenerationNumber });
     if (inserted) {
         summary.state = summary.aliveCells > 0 ? ConwayGenerationState::Good : ConwayGenerationState::NoCellsLeft;
     } else {
