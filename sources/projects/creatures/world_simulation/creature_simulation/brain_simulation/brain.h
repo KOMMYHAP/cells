84bf99d8b0b257d28bb070388eb96dc0b743f867
ppoliakov <ppoliakov@saber.games>
2026-05-23 16:41:39 +0300

    extract brain logic (keep it compiler-friendly)

---
+++ sources/projects/creatures/world_simulation/creature_simulation/brain_simulation/brain.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <array>
+
+/// Brain config
+/// @{
+inline constexpr size_t BrainInputCount = 10;
+
+inline constexpr size_t BrainHiddenNeuronsCount = 10;
+
+inline constexpr size_t BrainLinearOutputCount = 10;
+inline constexpr size_t BrainSigmoidOutputCount = 10;
+/// @}
+
+inline constexpr size_t BrainTotalOutputCount = BrainLinearOutputCount + BrainSigmoidOutputCount;
+
+struct BrainNeurons {
+    std::array<float, BrainInputCount> biasInputToHidden;
+    std::array<float, BrainHiddenNeuronsCount * BrainInputCount> weightInputToHidden;
+    std::array<float, BrainHiddenNeuronsCount> hiddenQuality;
+
+    std::array<float, BrainTotalOutputCount * BrainHiddenNeuronsCount> weightHiddenToOutput;
+    std::array<float, BrainTotalOutputCount> biasHiddenToOutput;
+};
+
+struct BrainInput {
+    std::array<float, BrainInputCount> values;
+};
+
+struct BrainOutput {
+    std::array<float, BrainLinearOutputCount> linearValues;
+    std::array<float, BrainSigmoidOutputCount> sigmoidValues;
+};
+
+void EvaluateBrain(ConstRef<BrainInput> brainInput, ConstRef<BrainNeurons> neurons, Ref<BrainOutput> output);
